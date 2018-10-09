e94bc3de75f1ab3cd1bf88de109bf2c84fade91b
Olivia <oosby@redventures.com>
2018-10-09 14:09:32 -0400

    tester-bot-

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * This is the entry point for your Probot App.
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+
+  app.on(`*`, async context => {
+    console.log('event ', context);
+    context.log({event: context.event, action: context.payload.action})
+    return context.github.issues.createComment('hello');
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
